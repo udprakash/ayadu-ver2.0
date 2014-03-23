a91a19ad49386b028acff08cf66a96b6b0c89ad1
udaya prakash <ud@thetraces.com>
2014-03-23 19:26:38 +0530

    pin details image details updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,17 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :pins
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :middle => "200x200>", :thumb => "100x100>" }
+
+  validates :firstname, presence: true, length: { maximum: 15 }
+  validates :secondname, presence: true, length: { maximum: 15 }
+  validates :phone, numericality: true
+  validates :street, presence: true
+  validates :locality, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :about, presence: true, length: { maximum: 500 }
+  validates :avatar, presence: true
+  
+  validates :pincode, numericality: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140318041258) do
+ActiveRecord::Schema.define(version: 20140321075117) do
 
   create_table "pins", force: true do |t|
     t.string   "title"
@@ -43,6 +43,20 @@ ActiveRecord::Schema.define(version: 20140318041258) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "firstname"
+    t.string   "secondname"
+    t.integer  "age"
+    t.text     "street"
+    t.string   "locality"
+    t.string   "city"
+    t.integer  "pincode"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
+    t.integer  "phone"
+    t.string   "state"
+    t.text     "about"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
